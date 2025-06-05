9ca4ddd47fd80515802d500d5e34295b55b36e79
Luis Molina <luismolina.wt11@gmail.com>
2025-06-05 15:23:53 +0000

    Quisiera que a la vista de administrador, un boton para gestionar a los

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -483,7 +483,10 @@ export default function HomePage() {
                         <UserPlus className="mr-2 h-4 w-4" />
                         Agregar Usuario
                       </Button>
-                      {/* Button to manage all users was here */}
+                      <Button onClick={() => setIsManageAllUsersDialogOpen(true)} variant="outline">
+                        <Users2 className="mr-2 h-4 w-4" />
+                        Gestionar Usuarios
+                      </Button>
                     </>
                   )}
                 </div>
